894bf4c0f786b7a5052d0796a77dab1fa5bb1372
tinnguyen189atnos <tinnguyen@nostech.vn>
2025-02-26 10:39:43 +0700

    [IMP] scaffold: added nostech template

---
+++ odoo/cli/templates/default-nostech/__init__.py
@@ -0,0 +1,4 @@
+from . import controllers
+from . import models
+from . import wizards
+# from .hooks import pre_init_hook, post_load, post_init_hook, uninstall_hook
---
+++ odoo/cli/templates/default-nostech/migrations/16.0.1.0/post-migrate.py
@@ -0,0 +1,5 @@
+from odoo import api, SUPERUSER_ID
+
+
+def migrate(cr, version):
+    env = api.Environment(cr, SUPERUSER_ID, {})
